12cdfafdc2510cbddb31c9a7c453495ee3844f84
alex <alex@alex-VirtualBox>
2019-05-02 16:16:06 +0200

    First Commit

---
+++ PrimoHomework/src/test/java/test/TestProva.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.text.ParseException;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TestProva 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TestProva( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TestProva.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     * @throws ParseException 
+     */
+    public void testApp() throws ParseException
+    {
+           
+    	
+    }
+}
