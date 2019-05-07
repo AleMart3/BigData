2f888e3b31c570d49311e3fc502a4840713ccb9a
giorgiamarini <giorgiaj.marini@gmail.com>
2019-05-07 19:17:53 +0200

    Modified ThirdReducer

--- PrimoHomework/src/main/java/job3/ThirdReducer.java
+++ PrimoHomework/src/main/java/job3/ThirdReducer.java
@@ -12,8 +12,7 @@ public class ThirdReducer extends Reducer<Text, ThirdMapperCustomValues, Text, T
 	public void reduce(Text key, Iterable<ThirdMapperCustomValues> values, Context context) throws IOException, InterruptedException {
 
 		List<ThirdMapperCustomValues> list = iterableToCollection(values);
-		List<ThirdMapperCustomValues> list2 = iterableToCollection(values);
-
+		
 		String[] valori = key.toString().split("[,]");
 		String a2016 = valori[0];
 		String a2017 = valori[1];
@@ -21,15 +20,13 @@ public class ThirdReducer extends Reducer<Text, ThirdMapperCustomValues, Text, T
 
 
 		for (int i = 0; i <list.size(); i++) {
-			int a = 1; 
-			for (int j = a; i <list2.size(); j++) {
-				if (list.get(i)!=list2.get(j) && list.get(i).getSettore()!=list2.get(j).getSettore()
-						&& list.get(i).getNome()!=list2.get(j).getNome()){
+			for (int j = i+1 ; j <list.size(); j++) {
+				if (list.get(i)!=list.get(j) && list.get(i).getSettore()!=list.get(j).getSettore()
+						&& list.get(i).getNome()!=list.get(j).getNome()){
 					context.write(new Text("Anno 2016: " + a2016 + "%, Anno 2017: " + a2017 + "%, Anno 2018: "+ a2018+ "%"), 
-							new Text(list.get(i)+ ", " + list2.get(j)));
+							new Text(list.get(i)+ ", " + list.get(j)));
 				}
 			}
-			a++; 
 		}
 		//context.write(new Text(key.getNome() + ", " + key.getSettore()), new Text(","+key.getAnno() +", "+  diff_percent));
 
