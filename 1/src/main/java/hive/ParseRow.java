e97c750feaa6cfdc3cea5472a74ef7b1b071c89f
AleMart3 <alex.3@live.it>
2019-05-10 15:34:26 +0200

    changes ParseRow

--- 1/src/main/java/hive/ParseRow.java
+++ 1/src/main/java/hive/ParseRow.java
@@ -1,20 +1,68 @@
 package hive;
 
-import org.apache.hadoop.hive.ql.exec.UDF;
-import org.apache.hadoop.io.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
 
 
 
-public class ParseRow extends UDF{
- 	public Text evaluate(Text text) {
- 	
-	String[] line = text.toString().split("[,]");
+public class ParseRow extends GenericUDTF {
+	 
+	  Integer count = Integer.valueOf(0);
+	  Object forwardObj[] = new Object[1];
+     
+	  private PrimitiveObjectInspector stringOI = null;
+	 
 	
-	return new Text(line[0] + line[line.length - 2 ]);
+	 
+	  @Override
+	  public StructObjectInspector initialize(ObjectInspector[] args) throws UDFArgumentException {
+		  
+		  stringOI = (PrimitiveObjectInspector) args[0];
+		  
+	      List<String> fieldNames = new ArrayList<String>(2);
+	      List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>(2);
 
-	
- 	
-	 }
+	      fieldNames.add("ticker");
+	      fieldNames.add("sector");
+	      fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+	      fieldOIs.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
+	      return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+	  }
+	 
+	  @Override
+	  public void process(Object[] record) throws HiveException {
+	    final String document = (String) stringOI.getPrimitiveJavaObject(record[0]);
+
+	    if (document == null) {
+	      return;
+	    }
+	    String[] line = document.split("[,]");
+	    
+	    String sector = line[line.length - 2 ];
+		
+		if (sector.contains((" \" ").trim())) 
+			sector = line[line.length - 3]; 
+	    
+	    
+	      forward(new Object[] { line[0], sector});
+	    
+	  }
+	  
+	  
+	  
+
+	  @Override
+	  public void close() throws HiveException {
+	    // do nothing
+	  }
 }
-
-
